ce2a59b5dc4408f8dfe1d5719aba23461fa52a61
RithikRaj64 <rithu0644@gmail.com>
2024-01-28 18:14:02 +0530

    Linked List Cycle 1 (Floyd's Hare and Tortoise)

---
+++ Data Structures/LLCycle1.java
@@ -0,0 +1,98 @@
+// Floyd's Tortoise and Hare algorithm
+
+// - The tortoise and hare start at the same position.
+// - The hare moves two steps at a time, and the tortoise moves one step at a time.
+// - If there is a cycle, the hare will eventually catch up to the tortoise.
+// - If there is no cycle, the hare will reach the end of the list.
+
+public class LLCycle1 {
+    public static void main(String[] args) {
+        CLinkedList ll = new CLinkedList();
+
+        ll.insert(1);
+        ll.insert(2);
+        ll.insert(3);
+        ll.insert(4);
+        ll.insert(5);
+
+        ll.print();
+
+        if(ll.hasCycle()) {
+            System.out.println("Cycle detected");
+        }
+        else {
+            System.out.println("No cycle detected");
+        }
+
+        ll.head.next.next.next.next.next = ll.head.next.next;
+
+        if(ll.hasCycle()) {
+            System.out.println("Cycle detected");
+        }
+        else {
+            System.out.println("No cycle detected");
+        }
+    }
+}
+
+class CNode {
+    int data;
+    CNode next;
+    CNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class CLinkedList {
+    CNode head;
+
+    CLinkedList() {
+        this.head = null;
+    }
+
+    void insert(int data) {
+        CNode nn = new CNode(data);
+
+        if(head == null) {
+            head = nn;
+        }
+
+        else {
+            CNode temp = head;
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = nn;
+        }
+    }
+
+    void print() {
+        CNode temp = this.head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    boolean hasCycle() {
+        if(head == null || head.next == null) return false;
+
+        CNode tortoise = head;
+        CNode hare = head.next;
+
+        while(tortoise != hare) {
+            if(hare == null || hare.next == null) return false;
+
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+        }
+
+        return true;
+    }
+}
